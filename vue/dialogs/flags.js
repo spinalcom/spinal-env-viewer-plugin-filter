e9d2262b5d22f70b57609aae20e26a0db980b952
Moussa Baradji <mbaradji221@outlook.fr>
2020-05-13 20:43:36 +0200

    Update flags.js

--- vue/dialogs/flags.js
+++ vue/dialogs/flags.js
@@ -16,43 +16,8 @@ export default [
     description: "Case insensitive match"
   },
   {
-    name: "extended",
-    value: "x",
-    description: "Ignore whitespace"
-  },
-  {
-    name: "extra",
-    value: "X",
-    description: "Disallow meaningless escapes"
-  },
-  {
-    name: "single line",
-    value: "s",
-    description: "Dot matches newline"
-  },
-  {
     name: "unicode",
     value: "u",
     description: "Match with full unicode"
-  },
-  {
-    name: "extngreedyra",
-    value: "U",
-    description: "Make quantifiers lazy"
-  },
-  {
-    name: "Anchored",
-    value: "A",
-    description: "Anchor to start of pattern"
-  },
-  {
-    name: "Jchanged",
-    value: "J",
-    description: "Allow duplicate subpattern names"
-  },
-  {
-    name: "Dollar end only",
-    value: "$",
-    description: "matches only end of pattern"
   }
 ]
